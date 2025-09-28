952bf11dedb8038ea4c67dba9cb2b31c1326a974
Ainhoa Menchén Jareño <ainhoamenchenjareno@MacBook-Air-de-Ainhoa.local>
2025-09-28 19:24:09 +0200

    Ajuste para Render: usar environment variables

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from dotenv import load_dotenv
 import os
 from datetime import datetime
 
 # Cargar variables de entorno
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-REPORT_CHANNEL_ID = int(os.getenv("REPORT_CHANNEL_ID"))
+TOKEN = os.getenv("BOT_TOKEN")
+REPORT_CHANNEL_ID = int(os.getenv(["REPORT_CHANNEL_ID"])
 
 intents = discord.Intents.default()
 intents.message_content = True
